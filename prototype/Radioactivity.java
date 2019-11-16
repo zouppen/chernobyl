76338108fef572a1be5d3ef7e7048c39be212462
Joel Lehtonen <joel.lehtonen@iki.fi>
2019-11-16 13:17:39 +0200

    Calculating dose and activity

--- prototype/Radioactivity.java
+++ prototype/Radioactivity.java
@@ -11,12 +11,25 @@ public class Radioactivity implements Runnable {
     }
     
     public void run() {
+	// Dose is energy (joules)
+	double dose = 0;
+	
 	while (true) {
+	    // totalPower is power of all beacons combined (watts)
+	    double totalPower = 0;
+	    
 	    System.out.println("RSSI dump");
 	    for (Integer signal : rssiMap.values()) {
 		System.out.println("RSSI " + signal);
-	    }
+		if (signal == null) continue; // Beacon not yet seen
 
+		double power = Math.pow(10, (double)signal/10);
+		totalPower += power;
+		System.out.println("Beacon power: "+power);
+	    }
+	    dose += totalPower;
+	    System.out.println("Power: "+totalPower+ " dose: " + dose);
+	    
 	    try {
 		Thread.sleep(1000);
 	    } catch (InterruptedException e) {
